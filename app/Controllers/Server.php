a6f049ce4602a91cf71345a4cca5d4d772b783d5
sirWojciechVojas <wojciech_vojas@o2.pl>
2021-01-29 07:49:22 +0100

    plik server - KeepAlive

--- app/Controllers/Server.php
+++ app/Controllers/Server.php
@@ -1,6 +1,7 @@
 <?php namespace App\Controllers;
 
 use Ratchet\Server\IoServer;
+use Ratchet\Http\OriginCheck;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use App\Libraries\Chat;
@@ -10,14 +11,31 @@ class Server extends BaseController
 	public function index()
 	{
 
+		//$loop = React\EventLoop\Factory::create();
+		//$loop->run();
+		$checkedApp = new WsServer( new Chat() );
+		//$checkedApp->enableKeepAlive($loop, 5);
+		//$checkedApp = new OriginCheck(new WsServer( new Chat() ), array('localhost'));
+		$checkedAppToPass = new OriginCheck($checkedApp);
+		$checkedAppToPass->allowedOrigins[] = BASE;
+		// $checkedAppToPass->allowedOrigins[] = BASE_WS;
+
 		$server = IoServer::factory(
 			new HttpServer(
-				new WsServer(
-					new Chat()
-				)
+				$checkedAppToPass
 			),
 			PORT_WS
 		);
+		$checkedApp->enableKeepAlive($server->loop, 10);
+
+		//works, so loop works
+		/*
+		$server->loop->addPeriodicTimer(5, function () use ($server) {
+			foreach ($server->app->clients as $client) {
+					$client->send("hello client");
+			}
+		});
+		*/
 		$db = db_connect();
 		$builder = $db->table('connections');
 		$builder->where(['c_id >' => 0])->delete();
